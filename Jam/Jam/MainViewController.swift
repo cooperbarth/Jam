a4d96172f8377d9ef9573238b7d00800d456bb9a
Cooper Barth <coop10101@gmail.com>
2018-11-12 15:25:47 -0600

    Set up SWRevealViewController for Menu

---
+++ Jam/Jam/MainViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RootViewController.swift
+//  Jam
+//
+//  Created by Cooper Barth on 11/12/18.
+//  Copyright © 2018 Cooper Barth. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: SWRevealViewController {
+
+    @IBOutlet weak var menuButton:UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setUpRevealViewController()
+    }
+
+    func setUpRevealViewController() {
+        let revealViewController: SWRevealViewController? = self.revealViewController()
+        if revealViewController != nil {
+            menuButton.target = revealViewController
+            menuButton.action = #selector(self.revealToggle(_:))
+            view.addGestureRecognizer(revealViewController!.panGestureRecognizer())
+        }
+    }
+}
